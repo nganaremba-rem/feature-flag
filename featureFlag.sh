d39631cdc24b40d7cef1ae5fa4358aabad2dca7e
remku <83505774+remku@users.noreply.github.com>
2021-05-06 12:34:49 +0530

    Update featureFlag.sh

--- featureFlag.sh
+++ featureFlag.sh
@@ -9,31 +9,29 @@ if [ $? -ne 0 ]; then
   echo -e "${red}Root Permission Unavailable${white}"
   exit
 fi
-sudo mount -o remount,rw /
+### Setting Permission & Mounting ###
 sudo mount -o remount,rw /vendor
 ### Backing UP ###
-if [ -e "$file.backup" ]; then
-  echo -e "${red}---> Backup Exist${white}"
-else
-  sudo cp $file $file.backup
-  echo -e "${green}---> $file Backup Completed${white}"
-fi
+sudo [ -e $file.backup ] && echo -e "${red}---> Backup Exist${white}" || sudo cp $file $file.backup; echo -e "${green}---> $file Backup Completed${white}"
+### Checking ON of OFF ###
 sudo cat $file | grep "persist.sys.fflag.override.settings_fuse=true" &> /dev/null
 if [ $? -ne 0 ]; then
   echo -e "persist.sys.fflag.override.settings_fuse=${red}false${white}"
 else
   echo -e "persist.sys.fflag.override.settings_fuse=${green}true${white}"
 fi
+### Toggle Command ###
 read -n1 -p $"Press 1 to toggle or Press 0 to exit" input
 if [ $input -eq 1 ]; then
-echo -e "${red}"
-sudo cat $file
+echo -e "${red}Default Last Line"
+sudo tail -1 $file
 echo -e "${white}"
-sudo echo "persist.sys.fflag.override.settings_fuse=true" >> $file
-echo -e "\n${green}"
-sudo cat $file
+### Writing lines to /vendor/build.prop ####
+sudo echo "persist.sys.fflag.override.settings_fuse=true" | sudo tee -a $file > /dev/null
+echo -e "\n${green}Last line after changes"
+sudo tail -1 $file
 echo -e "${white}"
 fi
-sudo mount -o remount,ro /vendor
-sudo mount -o remount,ro /
+### Default Permission & Unmounting ####
+sudo mount -o remount,ro /vendor 
 echo
