693cf3ec12f1393a2adfa5d66ad372efe30c56df
remku <83505774+remku@users.noreply.github.com>
2021-05-06 09:44:05 +0530

    Update featureFlag.sh

--- featureFlag.sh
+++ featureFlag.sh
@@ -10,7 +10,22 @@ if [ $? -ne 0 ]; then
   exit
 fi
 
-sudo mount -o remount,rw /
+sudo mount -o remount,rw /vendor
+### Backing UP ###
+if [ -e "$file.backup" ]; then
+  echo -e "${red}---> Backup Exist${white}"
+else
+  sudo cp $file $file.backup
+  echo -e "${green}---> $file Backup Completed${white}"
+fi
+sudo cat $file | grep "persist.sys.fflag.override.settings_fuse=true" &> /dev/null
+if [ $? -ne 0 ]; then
+  echo -e "persist.sys.fflag.override.settings_fuse=${red}false${white}"
+else
+  echo -e "persist.sys.fflag.override.settings_fuse=${green}true${white}"
+fi
+read -n1 -p $"\nPress 1 to toggle or Press 0 to exit" input
+if [ $input -eq 1 ]; then
 echo -e "${red}"
 sudo cat $file
 echo -e "${white}"
@@ -18,6 +33,6 @@ sudo echo "persist.sys.fflag.override.settings_fuse=true" >> $file
 echo -e "\n${green}"
 sudo cat $file
 echo -e "${white}"
-
-sudo mount -o remount,ro /
+fi
+sudo mount -o remount,ro /vendor
   
