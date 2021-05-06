e5b3e5ea6c7ab938472f9d62c64c660a7a3622b1
remku <83505774+remku@users.noreply.github.com>
2021-05-06 09:21:45 +0530

    Update featureFlag.sh

--- featureFlag.sh
+++ featureFlag.sh
@@ -3,20 +3,21 @@ red="\e[1;91m"
 green="\e[1;92m"
 white="\e[0m"
 file="/vendor/build.prop"
+sudo="su -c"
 su -v &> /dev/null
 if [ $? -ne 0 ]; then
   echo -e "${red}Root Permission Unavailable${white}"
   exit
 fi
 
-su -c mount -o remount,rw /
+sudo mount -o remount,rw /
 echo -e "${red}"
-cat $file
+sudo cat $file
 echo -e "${white}"
-echo "persist.sys.fflag.override.settings_fuse=true" >> $file
+sudo echo "persist.sys.fflag.override.settings_fuse=true" >> $file
 echo -e "\n${green}"
-cat $file
+sudo cat $file
 echo -e "${white}"
 
-su -c mount -o remount,ro /
+sudo mount -o remount,ro /
   
